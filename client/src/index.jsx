7379c8972456129d76774278b19cab33e38868e5
Ye Feng Chen <yefengchen96@gmail.com>
2021-10-08 14:13:54 -0400

    init

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div id='main'>
+      Hello World
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+const app = express()
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+app.use(express.json());
+app.use(morgan('dev'));
+
+app.listen(port, (err,something) => {
+  console.log(`Server running on port ${port}`);
+})
