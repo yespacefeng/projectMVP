f163aab39acb4aac07b3e4e31dc5d7b3d9f00162
Ye Feng Chen <yefengchen96@gmail.com>
2021-10-12 22:16:40 -0400

    Day 2 Complete

--- client/src/Spendings.jsx
+++ client/src/Spendings.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import c3 from 'c3';
 
-const Spendings = ({ transactions }) => (
-  <div id='spending-chart-container'>
-    <div id='chart'>
+class Spendings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions: ['spendings', ...props.transactions]
+    };
+    console.log(props);
+  }
 
-    </div>
-  </div>
-)
+  renderChart() {
+    console.log(this.state.transactions);
+    c3.generate({
+      bindto: '#chart',
+      data: {
+        columns: [this.state.transactions]
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.renderChart();
+  }
+
+  componentDidUpdate() {
+    this.renderChart();
+  }
+
+  render () {
+    return (
+      <div id='spending-chart-container'>
+        <h2>Spendings</h2>
+        <div id='chart'>
+
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Spendings;
